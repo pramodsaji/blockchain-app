8113aa38c846434f55ecfcd0868cc19521a59220
Riley <lifeiwang233@gmail.com>
2023-05-20 21:31:49 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,108 +1,155 @@
 import React, { useState, useEffect } from "react";
 import Web3 from "web3";
-import DataStorageContract from "./contracts/DataStorageContract.json";
+import DocumentVerificationContract from "./contracts/DocumentVerificationContract.json";
+import { SHA256 } from 'crypto-js';
+import './App.css';
 
 function App() {
-  const [id, setId] = useState("");
+
   const [name, setName] = useState("");
   const [data, setData] = useState("");
 
+  const [file, setFile] = useState();
+  const [sid, setSID] = useState('');
+  const [address, setAddress] = useState('');
+
   var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
-  var contract = null;
-  useEffect(() => {
-    loadBlockchainData();
-  }, []);
+  const handleSIDChange = (event) => {
+    setSID(event.target.value);
+  };
 
-  // You can now interact with the contract methods
-  // For example, to call the getData function:
-  const getDataFromContract = async () => {
+  function handleFile(event) {
+    setFile(event.target.files[0]);
+  }
+
+  var contract = null;
+  // useEffect(() => {
+  //   loadBlockchainData();
+  // }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     try {
+      // Perform form validation
+      if (!validateSID(sid)) {
+        alert('Please enter a valid SID.');
+        return;
+      }
+
+
+
+      // Generate hash of sid and file content together
+      const combinedData = `${sid}${getFileContent()}`;
+      const hash = SHA256(combinedData).toString();
+
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = DataStorageContract.networks[networkId];
+      const deployedNetwork = DocumentVerificationContract.networks[networkId];
 
       contract = new web3.eth.Contract(
-        DataStorageContract.abi,
+        DocumentVerificationContract.abi,
         deployedNetwork && deployedNetwork.address
       );
-      if (id !== "") {
-        const result = await contract.methods.getData(id).call();
-        setData(result);
-      }
+        console.log(hash);
+        console.log(address);
+      const result = await contract.methods.submitDocument(hash, address).send({ from: "0x35219cb510424B83b1f37640C93802Aa536B9915" });
+      console.log(result);
     } catch (error) {
       console.error("Error retrieving data:", error);
     }
+
+
   };
 
-  const loadBlockchainData = async () => {
-    try {
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = DataStorageContract.networks[networkId];
-
-      contract = new web3.eth.Contract(
-        DataStorageContract.abi,
-        deployedNetwork && deployedNetwork.address
-      );
-
-      getDataFromContract();
-    } catch (error) {
-      console.error("Error loading blockchain data:", error);
-    }
+  const validateSID = (sid) => {
+    // SID validation: Must be a positive number
+    const sidNumber = parseFloat(sid);
+    return !isNaN(sidNumber) && sidNumber > 0;
   };
 
-  const handleAddData = async () => {
-    try {
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = DataStorageContract.networks[networkId];
+  function getFileContent() {
+    return new Promise((resolve, reject) => {
+      if (!file) {
+        reject(new Error('No file selected.'));
+      }
 
-      const contract = new web3.eth.Contract(
-        DataStorageContract.abi,
-        deployedNetwork && deployedNetwork.address
-      );
-
-      await contract.methods
-        .addData(id, name)
-        .send({ from: web3.eth.defaultAccount });
-    } catch (error) {
-      console.error("Error adding data:", error);
-    }
+      const fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        const fileContent = e.target.result;
+        resolve(fileContent);
+      };
+      fileReader.onerror = (e) => {
+        reject(new Error('Error reading the file.'));
+      };
+      fileReader.readAsText(file);
+    });
   };
 
-  const handleGetData = () => {
-    // Call the getDataFromContract function or any other method to retrieve data
-    getDataFromContract();
-  };
+  const handleUpload = async () => {
+    const combinedData = `${sid}${getFileContent()}`;
+    const hash = SHA256(combinedData).toString();
 
-  return (
-    <div className="App">
-      <h1>Data Storage App</h1>
-      <div>
-        <label>ID:</label>
-        <input
-          type="number"
-          value={id}
-          onChange={(e) => setId(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Name:</label>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <button onClick={handleAddData}>Add Data</button>
-      <button onClick={handleGetData}>Get Data</button>
-      {data && (
-        <div>
-          <p>
-            Data: ID - {data[0]}, Name - {data[1]}
-          </p>
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = DocumentVerificationContract.networks[networkId];
+
+    contract = new web3.eth.Contract(
+      DocumentVerificationContract.abi,
+      deployedNetwork && deployedNetwork.address
+    );
+
+    console.log('hash + ' + hash);
+
+    const result = await contract.methods.verifyDocument(hash).call();
+    console.log('hi'+ result);
+  }
+
+
+return (
+  <div className="container">
+    <div className="sub-container">
+      <h2>Student Information</h2>
+      {/* <form onSubmit={handleSubmit}> */}
+
+        <div className="form-group">
+          <label htmlFor="sid">SID:</label>
+          <input type="text" id="sid" name="sid" value={sid} onChange={handleSIDChange} />
         </div>
-      )}
-    </div>
-  );
-}
 
+        <div className="form-group">
+          <label htmlFor="address">Address:</label>
+          <select id="address" name="address" value={address} onChange={(e) => setAddress(e.target.value)}>
+            <option value="">Select an address</option>
+            <option value="0x3e0604a8429bA70C19a7a5c728662dABe1F0969B">0x3e0604a8429bA70C19a7a5c728662dABe1F0969B</option>
+            <option value="0x82dAf97698cD8C022fD93FBC27ea3bF926A8CCa2">Address 2</option>
+            <option value="Address 3">Address 3</option>
+          </select>
+          <input type="file" name="file" onChange={handleFile} className="file-input" />
+        </div>
+
+        <div className="login-button">
+          <button type="submit" onClick={handleSubmit}>Submit</button>
+        </div>
+
+      {/* </form> */}
+
+      <div>
+        <h2>Verify File</h2>
+        {/* <form onClick={handleUpload}> */}
+          <input type="file" name="file" onChange={handleFile} className="file-input" />
+          <div className="login-button">
+            <button onClick={handleUpload}>Verify</button>
+          </div>
+        {/* </form> */}
+      </div>
+
+
+    </div>
+
+
+
+  </div>
+
+
+)
+}
 export default App;
